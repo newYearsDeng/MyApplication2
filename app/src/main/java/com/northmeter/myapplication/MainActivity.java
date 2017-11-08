306626eb329a1333ac8d28c5dfab69f0bd7ec8d5
newYearsDeng <905087116@qq.com>
2017-11-08 17:47:53 +0800

    第一次修改

--- app/src/main/java/com/northmeter/myapplication/MainActivity.java
+++ app/src/main/java/com/northmeter/myapplication/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                System.out.println("这是检测修改");
+                System.out.println("这是检测修改");
+                System.out.println("这是检测修改");
+                System.out.println("这是检测修改");
+                System.out.println("这是检测修改");
             }
         });
 
