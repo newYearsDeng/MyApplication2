c3a97748178658933d25d70027c1c1f6022f0390
newYearsDeng <33473612+newYearsDeng@users.noreply.github.com>
2017-11-09 17:52:28 +0800

    Update MainActivity.java

--- app/src/main/java/com/northmeter/myapplication/MainActivity.java
+++ app/src/main/java/com/northmeter/myapplication/MainActivity.java
@@ -41,7 +41,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        System.out.println("这是主分支");
+        System.out.println("这是主分支==");
+        System.out.println("这是主分支==");
     }
 
     @Override
